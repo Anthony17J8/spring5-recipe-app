22f52880cbbc49b8560602f2c0e790cd16eaba47
Anton Kiselev <j8kiselev@gmail.com>
2019-09-30 22:17:32 +0300

    Completed assignment of OneToOne mapping

--- src/main/java/com/ico/ltd/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/com/ico/ltd/spring5recipeapp/domain/Ingredient.java
@@ -1,10 +1,12 @@
 package com.ico.ltd.spring5recipeapp.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import java.math.BigDecimal;
 
 @Entity
@@ -20,6 +22,9 @@ public class Ingredient {
 
     @ManyToOne
     private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     public Long getId() {
         return id;
@@ -51,5 +56,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }

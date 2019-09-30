b3f959530e628c9f19de5e7d2eb74854d195ab71
Anton Kiselev <j8kiselev@gmail.com>
2019-09-30 23:09:53 +0300

    Overview JPA Enumeration

---
+++ src/main/java/com/ico/ltd/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.ico.ltd.spring5recipeapp.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/com/ico/ltd/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/ico/ltd/spring5recipeapp/domain/Recipe.java
@@ -2,6 +2,8 @@ package com.ico.ltd.spring5recipeapp.domain;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -30,8 +32,9 @@ public class Recipe {
     private String url;
 
     private String directions;
-    // todo add
-    // private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -128,5 +131,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }

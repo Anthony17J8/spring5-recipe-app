432f799c3143046ce0a4447edd40b265c0616a71
Anton Kiselev <j8kiselev@gmail.com>
2019-10-10 21:10:45 +0300

    Simple JUnit test. Using Mockito

---
+++ src/test/java/com/ico/ltd/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.ico.ltd.spring5recipeapp.services;
+
+import com.ico.ltd.spring5recipeapp.domain.Recipe;
+import com.ico.ltd.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+}

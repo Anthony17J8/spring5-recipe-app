efefd094f3d7129259d185888d7aeae99e5f59c0
Anton Kiselev <j8kiselev@gmail.com>
2019-11-11 17:11:35 +0300

    Handle Number format exception. Assignment

--- src/main/java/com/ico/ltd/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/ico/ltd/spring5recipeapp/controllers/RecipeController.java
@@ -75,4 +75,18 @@ public class RecipeController {
 
         return modelAndView;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exc) {
+        log.error("Handling number format exception");
+        log.error(exc.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.addObject("exc", exc);
+        modelAndView.setViewName("400error");
+
+        return modelAndView;
+    }
 }
--- src/test/java/com/ico/ltd/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/ico/ltd/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -107,4 +107,11 @@ public class RecipeControllerTest {
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
     }
+
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+        mockMvc.perform(get("/recipe/inval/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 }

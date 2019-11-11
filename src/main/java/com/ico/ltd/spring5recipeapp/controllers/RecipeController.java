bb50e8f7e1af3e9be2db0de0a77d8e672567c0ad
Anton Kiselev <j8kiselev@gmail.com>
2019-11-11 15:26:33 +0300

    Exception handler example

--- src/main/java/com/ico/ltd/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/ico/ltd/spring5recipeapp/controllers/RecipeController.java
@@ -1,15 +1,20 @@
 package com.ico.ltd.spring5recipeapp.controllers;
 
 import com.ico.ltd.spring5recipeapp.commands.RecipeCommand;
+import com.ico.ltd.spring5recipeapp.exceptions.NotFoundException;
 import com.ico.ltd.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Slf4j
@@ -55,5 +60,17 @@ public class RecipeController {
         log.debug("Deleting id:" + id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }

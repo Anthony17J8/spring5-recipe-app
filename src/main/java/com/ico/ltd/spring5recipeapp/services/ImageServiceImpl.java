fac8eb42d764b3897ae5c3b1493580d3276549f2
Anton Kiselev <j8kiselev@gmail.com>
2020-01-08 15:09:01 +0300

    Refactor Image Service

--- src/main/java/com/ico/ltd/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/ico/ltd/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,12 +1,12 @@
 package com.ico.ltd.spring5recipeapp.services;
 
 import com.ico.ltd.spring5recipeapp.domain.Recipe;
-import com.ico.ltd.spring5recipeapp.repositories.RecipeRepository;
+import com.ico.ltd.spring5recipeapp.repositories.reactive.RecipeReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 
@@ -14,35 +14,34 @@ import java.io.IOException;
 @Service
 public class ImageServiceImpl implements ImageService {
 
-    private RecipeRepository recipeRepository;
+    private final RecipeReactiveRepository recipeReactiveRepository;
 
     @Autowired
-    public ImageServiceImpl(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
+    public ImageServiceImpl(RecipeReactiveRepository recipeReactiveRepository) {
+        this.recipeReactiveRepository = recipeReactiveRepository;
     }
 
     @Override
-    @Transactional
-    public void saveImageFile(String recipeId, MultipartFile file) {
-        try {
-            Recipe recipe = recipeRepository.findById(recipeId).get();
+    public Mono<Void> saveImageFile(String recipeId, MultipartFile file) {
+        Mono<Recipe> recipeMono = recipeReactiveRepository.findById(recipeId)
+                .map(recipe -> {
+                    try {
+                        Byte[] bytes = new Byte[file.getBytes().length];
 
-            Byte[] bytes = new Byte[file.getBytes().length];
+                        int i = 0;
 
-            int i = 0;
+                        for (byte b : file.getBytes()) {
+                            bytes[i++] = b;
+                        }
+                        recipe.setImage(bytes);
+                        return recipe;
+                    } catch (IOException exc) {
+                        exc.printStackTrace();
+                        throw new RuntimeException(exc);
+                    }
+                });
 
-            for (byte b : file.getBytes()) {
-                bytes[i++] = b;
-            }
-
-            recipe.setImage(bytes);
-
-            recipeRepository.save(recipe);
-
-        } catch (IOException exc) {
-            //todo handle better
-            log.error("Error occurred", exc);
-            exc.printStackTrace();
-        }
+        recipeReactiveRepository.save(recipeMono.block()).block();
+        return Mono.empty();
     }
 }
--- src/test/java/com/ico/ltd/spring5recipeapp/services/ImageServiceImplTest.java
+++ src/test/java/com/ico/ltd/spring5recipeapp/services/ImageServiceImplTest.java
@@ -1,7 +1,7 @@
 package com.ico.ltd.spring5recipeapp.services;
 
 import com.ico.ltd.spring5recipeapp.domain.Recipe;
-import com.ico.ltd.spring5recipeapp.repositories.RecipeRepository;
+import com.ico.ltd.spring5recipeapp.repositories.reactive.RecipeReactiveRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -9,10 +9,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Optional;
+import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.when;
 public class ImageServiceImplTest {
 
     @Mock
-    private RecipeRepository recipeRepository;
+    private RecipeReactiveRepository recipeReactiveRepository;
 
     private ImageService imageService;
 
@@ -28,7 +28,7 @@ public class ImageServiceImplTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        imageService = new ImageServiceImpl(recipeRepository);
+        imageService = new ImageServiceImpl(recipeReactiveRepository);
     }
 
     @Test
@@ -40,9 +40,9 @@ public class ImageServiceImplTest {
 
         Recipe recipe = new Recipe();
         recipe.setId(id);
-        Optional<Recipe> recipeOptional = Optional.of(recipe);
 
-        when(recipeRepository.findById(anyString())).thenReturn(recipeOptional);
+        when(recipeReactiveRepository.findById(anyString())).thenReturn(Mono.just(recipe));
+        when(recipeReactiveRepository.save(any(Recipe.class))).thenReturn(Mono.just(recipe));
 
         ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
 
@@ -50,7 +50,7 @@ public class ImageServiceImplTest {
         imageService.saveImageFile(id, multipartFile);
 
         //then
-        verify(recipeRepository).save(argumentCaptor.capture());
+        verify(recipeReactiveRepository).save(argumentCaptor.capture());
         Recipe savedRecipe = argumentCaptor.getValue();
         assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
     }
